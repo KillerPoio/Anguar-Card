627d1e0238c0f21408db641f043065d6e61bd0f2
JuanPolo <juanpolo.jps@gmail.com>
2021-11-10 17:45:53 -0500

    inicio de la info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SupermanComponent } from './superman/superman.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SupermanComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
